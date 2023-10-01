287bd499693ec7cde811a365f62c9a7a4d40fd93
Ryan Stewart <ryanstewartalex@gmail.com>
2023-10-01 18:03:55 -0500

    Update

---
+++ main.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 3000;
+
+
+app.get("/", (req, res) =>
+{
+    res.sendFile(path.join(__dirname, "./views/index.html"))
+});
+
+
+app.listen(PORT, () =>
+{
+    console.log("Quickbin listening on port " + PORT);
+});
