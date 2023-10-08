ae959ae2bc52140ba9a8397907949e0ea1bef0c0
Ryan Stewart <ryanstewartalex@gmail.com>
2023-10-08 16:09:09 -0500

    Update editor.js

--- public/js/editor.js
+++ public/js/editor.js
@@ -3,9 +3,12 @@ const DEFAULT_LANGUAGE = "text";
 
 let theme = DEFAULT_THEME;
 let language = DEFAULT_LANGUAGE;
-
 const editor = ace.edit("editor");
-
 
 editor.setTheme(`ace/theme/${theme}`);
 editor.session.setMode(`ace/mode/${language}`);
+
+if (window.location.href.includes("/save/"))
+{
+    editor.setReadOnly(true);
+}
