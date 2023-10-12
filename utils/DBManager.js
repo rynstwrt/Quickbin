ec9c8d551754268e0dd57f7e391a3c79901ae941
Ryan Stewart <ryanstewartalex@gmail.com>
2023-10-11 19:53:37 -0500

    Add Ace Locally

--- utils/DBManager.js
+++ utils/DBManager.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    charset: "utf8"
+    charset: "utf8mb4"
 });
 
 
